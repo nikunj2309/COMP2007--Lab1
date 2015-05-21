83f56d00701dc5c181002a46d3b0d47af6fac0ae
Nikunj Vala <nikunj_2309@yahoo.com>
2015-05-21 10:12:59 -0400

    Edited Lab1.aspx.cs file

--- Lab1_updated/Lab1.aspx.cs
+++ Lab1_updated/Lab1.aspx.cs
@@ -5,13 +5,42 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Lab1_updated
+namespace Lab1
 {
     public partial class Lab1 : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lb_Name.Text = "Name: " + NameTextBox.Text;
+            lb_Password.Text = "Password: " + PasswordTextBox.Text;
+            lb_Address.Text = "Address: " + AddressTextBox.Text;
+            lb_Education.Text = "Education: " + EducationRbl.SelectedValue;
+            if (LaptopCheckBox.Checked)
+            {
+                lb_Laptop.Text = "Has Laptop: Yes ";
+            }
+            else
+            {
+                lb_Laptop.Text = "Has Laptop: No ";
+            }
+
+
+            //loop through skills checkbox list to build skill list dynamically
+            lb_Skills.Text = NameTextBox.Text + "  Have Following Skills: ";
+            foreach (ListItem item in SkillsChk.Items)
+            {
+                if (item.Selected)
+                {
+                    lb_Skills.Text += item.Text + " ";
+                }
+            }
+
+            lb_Province.Text = "Province  " + NameTextBox.Text + "  live in: " + Provincedd.SelectedValue;
         }
     }
 }
